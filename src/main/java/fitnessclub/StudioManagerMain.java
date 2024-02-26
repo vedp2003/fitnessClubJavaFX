4bb20bf0dc08e5207761511d9c0811b0cb7462b5
vedp2003 <vedp2003@gmail.com>
2024-02-26 16:11:58 -0500

    Added

--- src/main/java/fitnessclub/StudioManagerMain.java
+++ src/main/java/fitnessclub/StudioManagerMain.java
@@ -11,8 +11,13 @@ public class StudioManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StudioManagerMain.class.getResource("studioManagerView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 600, 600);
+        } catch ( IOException e ) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Project 3 - Studio Manager");
         stage.setScene(scene);
         stage.show();
     }
