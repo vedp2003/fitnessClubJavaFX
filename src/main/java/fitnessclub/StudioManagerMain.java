97c169387c6dfb40fe8c599e84a7e6558466a6ae
vedp2003 <vedp2003@gmail.com>
2024-02-26 12:35:34 -0500

    Added

--- src/main/java/fitnessclub/StudioManagerMain.java
+++ src/main/java/fitnessclub/StudioManagerMain.java
@@ -1,6 +1,23 @@
 package fitnessclub;
 
-public class StudioManagerMain {
-    //sdfihsd
-    //
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StudioManagerMain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StudioManagerMain.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
